1311abbfb800dbf630de94e376be9a1e1b411ed6
minju0611 <minju0185@naver.com>
2025-03-15 17:05:54 +0900

    feat : df명 변경

--- package/preprocessing/test_pipeline.py
+++ package/preprocessing/test_pipeline.py
@@ -1,11 +1,14 @@
 import pandas as pd
 
-def preprocessing_customer_data(df):
+def preprocessing_customer_data(df_21, df_22):
     """
     이탈 고객 라벨링 + 카드 건수 0인행 제거하는 공통 전처리 함수 
     """
-    df = df[df["PYE_C1M210000"] > 0]
-    df["이탈여부"] = (df["PYE_C18233003"] - df["PYE_C18233003"] > 0).astype(int)
-    return df
+    # 카드 건수(PYE_C1M210000) 0인 행 제거 (df_22 기준)
+    df_22 = df_22[df_22["PYE_C1M210000"] > 0]
+    # 이탈 여부 라벨링
+    df_22["이탈여부"] = (df_21["PYE_C18233005"] - df_22["PYE_C18233005"] > 0).astype(int)
+
+    return df_22
 
 
